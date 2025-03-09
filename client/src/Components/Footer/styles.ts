89ae9d7df141f6e8dbd91dec46ffb9f14c021f38
Joao123433 <joaosantos123433@gmail.com>
2025-03-09 02:21:51 -0300

    Created footer

---
+++ client/src/Components/Footer/styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const FooterContainer = styled.footer`
+  display: none;
+
+  @media (min-width: 1000px) {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: .8rem;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+
+    nav {
+      display: flex;
+      flex-direction: column;
+      gap: .4rem;
+    }
+  }
+`;
--- client/src/layout/DefaultLayout/index.tsx
+++ client/src/layout/DefaultLayout/index.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../../Components/Header";
 import { DefaultLayoutContainer } from "./styles";
+import { Footer } from "../../Components/Footer";
 
 export function DefaultLayout() {
   return (
     <DefaultLayoutContainer>
       <Header />
       <Outlet />
+      <Footer />
     </DefaultLayoutContainer>
   )
 }
